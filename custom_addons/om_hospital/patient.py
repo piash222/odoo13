358376b85bf3bd53393c8ae0c9d0ecf5674ed08d
Md. Shihab Uddin <piash222@gmail.com>
2020-12-20 18:03:06 +0600

    track field value with track_visibilty=always

--- custom_addons/om_hospital/patient.py
+++ custom_addons/om_hospital/patient.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, _, api
 
+
 class SalesOrderInherit(models.Model):
     _inherit = 'sale.order'
     patient_name = fields.Char(string='Patient Name')
@@ -9,6 +10,7 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Patient Record'
+
     @api.depends('patient_age')
     def set_age_group(self):
         for rec in self:
@@ -18,11 +20,9 @@ class HospitalPatient(models.Model):
                 else:
                     rec.age_group = "major"
 
-
-
     name = fields.Char(string='Test', required=True)
-    patient_name = fields.Char(string='Name', required=True)
-    patient_age = fields.Integer(string='age')
+    patient_name = fields.Char(string='Name', required=True, track_visibility='always')
+    patient_age = fields.Integer(string='age', track_visibility='always')
     gender = fields.Selection([
         ('male', 'Male'),
         ('fe_male', 'Female'),
@@ -36,7 +36,7 @@ class HospitalPatient(models.Model):
     notes = fields.Text(string='Notes')
     image = fields.Binary(string='Image')
     name_seq = fields.Char(string='Order Reference', required=True, copy=False, readonly=True,
-                        index=True, default=lambda self: _('New'))
+                           index=True, default=lambda self: _('New'))
 
     @api.model
     def create(self, vals):
