04396956955907ed7f26e76b189e0f6d691ee191
Md. Shihab Uddin <piash222@gmail.com>
2020-12-20 17:47:10 +0600

    compute field and its function

--- custom_addons/om_hospital/patient.py
+++ custom_addons/om_hospital/patient.py
@@ -9,6 +9,16 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Patient Record'
+    @api.depends('patient_age')
+    def set_age_group(self):
+        for rec in self:
+            if rec.patient_age:
+                if rec.patient_age < 18:
+                    rec.age_group = "minor"
+                else:
+                    rec.age_group = "major"
+
+
 
     name = fields.Char(string='Test', required=True)
     patient_name = fields.Char(string='Name', required=True)
@@ -17,6 +27,12 @@ class HospitalPatient(models.Model):
         ('male', 'Male'),
         ('fe_male', 'Female'),
     ], default='male', string="Gender")
+
+    age_group = fields.Selection([
+        ('major', 'Major'),
+        ('minor', 'Minor'),
+    ], string="Age Group", compute='set_age_group')
+
     notes = fields.Text(string='Notes')
     image = fields.Binary(string='Image')
     name_seq = fields.Char(string='Order Reference', required=True, copy=False, readonly=True,
