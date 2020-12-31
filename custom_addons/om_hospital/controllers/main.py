bbb0fd1243952e1c6f078b4ebf143ff7f0fae4b9
Md. Shihab Uddin <piash222@gmail.com>
2020-12-31 12:54:18 +0600

    with controller api request(create, red, auth) maintained

--- custom_addons/om_hospital/controllers/main.py
+++ custom_addons/om_hospital/controllers/main.py
@@ -10,3 +10,36 @@ class Hospital(http.Controller):
         return http.request.render('om_hospital.patients_page', {
             'patients': patients
         })
+
+    @http.route('/create_patient', type='json', auth='user')
+    def create_patient(self, **rec):
+        if http.request.jsonrequest:
+            print('rec', rec)
+            if rec['name']:
+                vals = {
+                    'patient_name': rec['name']
+                }
+                new_patient = http.request.env['hospital.patient'].sudo().create(vals)
+                print(new_patient, 'is the patient')
+                args = {
+                    'success': True, 'message': 'Success', 'ID': new_patient.id
+                }
+                return args
+
+    @http.route('/get_patients', type='json', auth='user')
+    def get_patient(self):
+        print('yes here entered')
+        patient_rec = http.request.env['hospital.patient'].search([])
+        patients = []
+        for rec in patient_rec:
+            vals = {
+                'id': rec.id,
+                'name': rec.patient_name,
+                'sequence': rec.name_seq
+            }
+            patients.append(vals)
+        print(patients, '<------patient list')
+        data = {
+            'status': 200, 'response': patients, 'messages': 'Done All Patients Returned'
+        }
+        return data
