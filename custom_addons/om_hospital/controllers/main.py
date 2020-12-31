8995bd2f11abf3303f0d0802500f0d29846c10f8
Md. Shihab Uddin <piash222@gmail.com>
2020-12-31 15:28:48 +0600

    inherit existing controller

--- custom_addons/om_hospital/controllers/main.py
+++ custom_addons/om_hospital/controllers/main.py
@@ -1,4 +1,18 @@
 from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleInherit(WebsiteSale):
+    @http.route([
+        '''/shop''',
+        '''/shop/page/<int:page>''',
+        '''/shop/category/<model("product.public.category"):category>''',
+        '''/shop/category/<model("product.public.category"):category>/page/<int:page>'''
+    ], type='http', auth="public", website=True)
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
+        res = super(WebsiteSaleInherit, self).shop()
+        print('inherited odoo mates')
+        return res
 
 
 class Hospital(http.Controller):
